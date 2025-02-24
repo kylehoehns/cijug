6ef9e92543be5dbeaa17f9be89aaddd4762af9b8
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2025-02-24 11:41:57 -0600

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
-export const metadata: Metadata = {
-  title: 'CIJUG',
-  description: 'Central Iowa Java Users Group',
+export const metadata = {
+  title: "CIJUG",
+  description: "Central Iowa Java Users Group",
   generator: 'v0.dev',
-}
+  icons: {
+    icon: [
+      { url: "/cijug/favicon.ico", sizes: "any" },
+      { url: "/cijug/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/cijug/favicon-32x32.png", sizes: "32x32", type: "image/png" }
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
